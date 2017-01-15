a70625a5a8ce993076d87743044427808b56b559
Dujin Jeong <wjdenwls123@gmail.com>
2017-01-15 23:15:23 +0900

    test: update python_pipe
    Send, Get Msg twice time(many)

--- test_python_pipe/main.py
+++ test_python_pipe/main.py
@@ -5,7 +5,8 @@ GET STDOUT FROM OTHER PYTHON SCRIPT
 import subprocess
 from subprocess import PIPE, STDOUT
 
-player_pipe = subprocess.Popen(["source\call.py", 'arg1'], stdin=PIPE,
+# "-u" not to buffer the output of python
+player_pipe = subprocess.Popen(["source\call.py", "-u", 'arg1'], stdin=PIPE,
 	stdout=PIPE, stderr=STDOUT, shell=True)
 
 #grep_stdout = open_process.communicate()
@@ -14,6 +15,13 @@ player_pipe = subprocess.Popen(["source\call.py", 'arg1'], stdin=PIPE,
 player_pipe.stdin.write("Send Msg\n")
 get_stdout = player_pipe.stdout.readline()
 print("[Get Msg]" + get_stdout)
+
+
+
+player_pipe.stdin.write("Send Msg2\n")
+get_stdout = player_pipe.stdout.readline()
+print("[Get Msg]" + get_stdout)
+
 
 player_pipe.kill()
 player_pipe.wait()
--- test_python_pipe/source/call.py
+++ test_python_pipe/source/call.py
@@ -2,3 +2,8 @@ import sys
 
 getMsg = raw_input()
 print getMsg
+sys.stdout.flush()
+
+getMsg2 = raw_input()
+print getMsg2
+sys.stdout.flush()
